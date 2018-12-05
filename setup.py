9d723c919f48831b95f18debcde9c2bf23da4640
HSLee <peanutstars.lee@gmail.com>
2018-12-05 12:12:40 +0900

    Start dev-mapper.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='dev-mapper',
+    version='0.0.1',
+    description='Converter the sys-pci-address to a device file',
+    author='HyunSuk Lee',
+    author_email='peanutstars.job@gmail.com',
+    scripts=[
+    ],
+    keywords='dev path convert',
+    packages=['devmapper'],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Manufacturing',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=[
+    ],
+    setup_requires=[
+    ],
+)
